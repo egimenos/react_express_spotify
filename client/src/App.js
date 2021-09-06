a2f304557dc8a7c680fbc8fff331661ca5957d9d
EgimenoS <ergisan@gmail.com>
2021-09-06 20:11:32 +0200

    added spotify service to handle auth tokens logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,27 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import { accessToken, logout } from "./services/spotify";
 import "./App.css";
-import React from "react";
 
 function App() {
-  React.useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get("access_token");
-    const refreshToken = urlParams.get("refresh_token");
+  const [token, setToken] = useState(null);
 
-    console.log(accessToken);
-    console.log(refreshToken);
-
-    if (refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-        .then((res) => res.json())
-        .then((data) => console.log(data))
-        .catch((err) => console.error(err));
-    }
+  useEffect(() => {
+    setToken(accessToken);
   }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="http://localhost:8888/login">
-          Log in to Spotify
-        </a>
+        {!token ? (
+          <a className="App-link" href="http://localhost:8888/login">
+            Log in to Spotify
+          </a>
+        ) : (
+          <>
+            <h1>Logged in!</h1>
+            <button onClick={logout}>Log Out</button>
+          </>
+        )}
       </header>
     </div>
   );
