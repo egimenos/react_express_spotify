a6dec458a1ec278dd223afd28b36f1e25ff01adc
EgimenoS <ergisan@gmail.com>
2021-09-19 20:45:02 +0200

    started Profile Page

--- client/src/services/spotify.js
+++ client/src/services/spotify.js
@@ -9,4 +9,13 @@ axios.defaults.baseURL = "https://api.spotify.com/v1";
 axios.defaults.headers["Authorization"] = `Bearer ${accessToken}`;
 axios.defaults.headers["Content-Type"] = "application/json";
 
+/**
+ * Get a List of Current User's Playlists
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-a-list-of-current-users-playlists
+ * @returns {Promise}
+ */
+export const getCurrentUserPlaylists = (limit = 20) => {
+  return axios.get(`/me/playlists?limit=${limit}`);
+};
+
 export const getCurrentUserProfile = () => axios.get("/me");
