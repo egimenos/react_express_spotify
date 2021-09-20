0380c432100e25778cdb0460f1cc8b7aa500a1a4
EgimenoS <ergisan@gmail.com>
2021-09-20 19:53:10 +0200

    Added top tracks section to profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,14 +4,16 @@ import {
   getCurrentUserProfile,
   getCurrentUserPlaylists,
   getTopArtists,
+  getTopTracks,
 } from "../services/spotify";
 import { StyledHeader } from "../styles";
-import { SectionWrapper, ArtistsGrid } from "../components";
+import { SectionWrapper, ArtistsGrid, TrackList } from "../components";
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
   const [playlists, setPlaylists] = useState(null);
   const [topArtists, setTopArtists] = useState(null);
+  const [topTracks, setTopTracks] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,6 +25,9 @@ const Profile = () => {
 
       const userTopArtist = await getTopArtists();
       setTopArtists(userTopArtist.data);
+
+      const userTopTracks = await getTopTracks();
+      setTopTracks(userTopTracks.data);
     };
 
     catchErrors(fetchData());
@@ -59,13 +64,20 @@ const Profile = () => {
               </div>
             </div>
           </StyledHeader>
-          {topArtists && (
+          {topArtists && topTracks && (
             <main>
               <SectionWrapper
                 title="Top artists this month"
                 seeAllLink="/top-artists"
               >
                 <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
+              </SectionWrapper>
+
+              <SectionWrapper
+                title="Top tracks this month"
+                seeAllLink="/top-tracks"
+              >
+                <TrackList tracks={topTracks.items.slice(0, 10)} />
               </SectionWrapper>
             </main>
           )}
