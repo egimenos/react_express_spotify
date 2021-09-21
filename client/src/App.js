07553b797688b887d62333e0d4718053cc2bd038
EgimenoS <ergisan@gmail.com>
2021-09-21 20:27:00 +0200

    Added individual Playlist page

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,14 @@ import {
   useLocation,
 } from "react-router-dom";
 import { accessToken, logout } from "./services/auth";
-import { Login, Profile, TopArtists, TopTracks, Playlists } from "./pages";
+import {
+  Login,
+  Profile,
+  TopArtists,
+  TopTracks,
+  Playlists,
+  Playlist,
+} from "./pages";
 import { GlobalStyle } from "./styles";
 import styled from "styled-components/macro";
 
@@ -66,7 +73,7 @@ function App() {
                 <TopTracks />
               </Route>
               <Route path="/playlists/:id">
-                <h1>Playlist</h1>
+                <Playlist />
               </Route>
               <Route path="/playlists">
                 <Playlists />
